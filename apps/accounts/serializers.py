269a2bac1c1566aa788719ab27ab6dde2078ce97
weyprogramming <weyrowitzlennart@gmail.com>
2022-04-21 14:13:28 +0200

    First Commit

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,33 @@
+from rest_framework.serializers import ModelSerializer
+# from apps.measures.serializers import MeasurementSerializer
+
+from .models import Car, Account
+
+class CarSerializer(ModelSerializer):
+    class Meta:
+        model = Car
+        fields = [
+            'manufacturer',
+            'model_name',
+            'construction_year',
+            'fuel_type',
+            'mass_kg',
+            'engine_displacement_l',
+            'number_of_cylinders',
+            'milage_km',
+            'power_ps',
+        ]
+
+class AccountSerializer(ModelSerializer):
+    car = CarSerializer(required=False)
+
+    class Meta:
+        model = Account
+        fields = [
+            'first_name',
+            'last_name',
+            'date_of_birth',
+            'driving_license_since',
+            'gender',
+            'car',
+        ]
